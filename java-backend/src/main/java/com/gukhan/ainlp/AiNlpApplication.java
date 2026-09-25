32a00469f9b53dffba8ce63a85e65c9dcb659f27
Gukhan B <bgukhan204@gmail.com>
2026-09-25 20:14:33 +0530

    Add Java Spring Boot authentication and security foundation

---
+++ java-backend/src/main/java/com/gukhan/ainlp/AiNlpApplication.java
@@ -0,0 +1,3 @@
+package com.gukhan.ainlp;
+import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication public class AiNlpApplication{public static void main(String[] args){SpringApplication.run(AiNlpApplication.class,args);}}
